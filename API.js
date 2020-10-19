a234d76b2482fd61fd5e0fdd1c9b1c3d9efd2107
vistritpandey <vistritpandey@gmail.com>
2020-10-19 08:50:21 +0530

    added

---
+++ API.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBayp4CHV2xxJpoSRII0sR7TkqE5-ZEM6o",
+    authDomain: "vistrit-utube.firebaseapp.com",
+    databaseURL: "https://vistrit-utube.firebaseio.com",
+    projectId: "vistrit-utube",
+    storageBucket: "vistrit-utube.appspot.com",
+    messagingSenderId: "837589100335",
+    appId: "1:837589100335:web:ab8c2e3929c9b3bb42e3da",
+    measurementId: "G-VKVF9JQHXC"
+  };
