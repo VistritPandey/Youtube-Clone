0912c021cdb99c10ae2a96393a0a8774808c96fe
vistritpandey <vistritpandey@gmail.com>
2020-10-21 18:08:17 +0530

    m

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import './Sidebar.css'
+import SidebarRow from './SidebarRow'
 
 function Sidebar() {
     return (
         <div className="sidebar">
             <h1>I am the sidebar</h1>
+            <SidebarRow title="Home"/>
+            <SidebarRow title="Trending"/>
+            <SidebarRow title="Subscriptions"/>
         </div>
     )
 }
---
+++ src/SidebarRow.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './SidebarRow.css'
+function SidebarRow({ title }) {
+    return (
+        <div>
+            <h2> { title } </h2>
+        </div>
+    )
+} 
+export default SidebarRow
