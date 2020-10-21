b7d0ca120eaa8739b5d194dc02e8e9b48dba0cb1
vistritpandey <vistritpandey@gmail.com>
2020-10-21 16:57:43 +0530

    m

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import './Header.css'
 import MenuIcon from "@material-ui/icons/Menu"
+import SearchIcon from "@material-ui/icons/Search"
 
 function Header() {
     return (
         <div className="header">
             <MenuIcon />
             <img className="header__logo" src="https://www.clipartkey.com/mpngs/m/38-389855_youtube-red-transparent-background-youtube-icon.png" alt=""/>
-
+            <input type ="text"></input>
+            <SearchIcon />
         </div>
     )
 }
