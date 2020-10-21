d55a25964e56b9dfc6ef55e162a1ad9674b99152
vistritpandey <vistritpandey@gmail.com>
2020-10-21 17:49:09 +0530

    m

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './Header'
+import Sidebar from "./Sidebar"
 
 function App() {
   return (
     <div className="App">
       <Header />
-      
+      <Sidebar />
+
     </div>
   );
 }
