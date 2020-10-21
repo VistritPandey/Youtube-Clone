a1832fddc46c6b5208954a16e02647e1ce545d7b
vistritpandey <vistritpandey@gmail.com>
2020-10-21 17:01:06 +0530

    m

--- src/Header.js
+++ src/Header.js
@@ -3,6 +3,8 @@ import './Header.css'
 import MenuIcon from "@material-ui/icons/Menu"
 import SearchIcon from "@material-ui/icons/Search"
 import VideoCallIcon from "@material-ui/icons/VideoCall"
+import AppsIcon from "@material-ui/icons/Apps"
+import NotificationslIcon from "@material-ui/icons/Notifications"
 
 function Header() {
     return (
@@ -12,6 +14,8 @@ function Header() {
             <input type ="text"></input>
             <SearchIcon />
             <VideoCallIcon />
+            <AppsIcon />
+            <NotificationslIcon />
         </div>
     )
 }
