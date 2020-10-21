a3038484d092b79b4782cf5628c01aeddf3e9e50
vistritpandey <vistritpandey@gmail.com>
2020-10-21 16:56:17 +0530

    m

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import './Header.css'
+import MenuIcon from "@material-ui/icons/Menu"
 
 function Header() {
     return (
         <div className="header">
-            <h1>This is the header</h1>
+            <MenuIcon />
+            <img className="header__logo" src="https://www.clipartkey.com/mpngs/m/38-389855_youtube-red-transparent-background-youtube-icon.png" alt=""/>
+
         </div>
     )
 }
