c8271413e62a7b3114f2664e8307b26fa09da552
vistritpandey <vistritpandey@gmail.com>
2020-10-21 18:09:29 +0530

    m

--- src/SidebarRow.js
+++ src/SidebarRow.js
@@ -2,7 +2,8 @@ import React from 'react'
 import './SidebarRow.css'
 function SidebarRow({ title }) {
     return (
-        <div>
+        <div className='sidebarRow'>
+        
             <h2> { title } </h2>
         </div>
     )
