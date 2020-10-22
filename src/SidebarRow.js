5297d03100977c2991897c4ff2612d6f039631ec
vistritpandey <vistritpandey@gmail.com>
2020-10-22 18:34:28 +0530

    home

--- src/SidebarRow.js
+++ src/SidebarRow.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import './SidebarRow.css'
+import HomeIcon from '@material-ui/icons/Home'
 function SidebarRow({ title }) {
     return (
         <div className='sidebarRow'>
-        
+            <HomeIcon />
             <h2> { title } </h2>
         </div>
     )
