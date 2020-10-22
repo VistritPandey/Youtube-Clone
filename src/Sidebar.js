a3fd134d29cc7b4d13d4bf1dfe05a7595b37dfc9
vistritpandey <vistritpandey@gmail.com>
2020-10-22 19:14:27 +0530

    m

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -8,7 +8,7 @@ import SubscriptionsIcon from '@material-ui/icons/Subscriptions'
 function Sidebar() {
     return (
         <div className="sidebar">
-            <SidebarRow Icon={HomeIcon} title="Home"/>
+            <SidebarRow selected Icon={HomeIcon} title="Home"/>
             <SidebarRow Icon={WhatshotIcon} title="Trending"/>
             <SidebarRow Icon={SubscriptionsIcon} title="Subscriptions"/>
         </div>
--- src/SidebarRow.js
+++ src/SidebarRow.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import './SidebarRow.css'
-function SidebarRow({ Icon, title }) {
+
+function SidebarRow({ selected, Icon, title }) {
     return (
-        <div className='sidebarRow'>
-            <Icon />
-            <h2> { title } </h2>
+        <div className={`sidebarRow ${selected && "selected"}`}>
+            <Icon className="sidebarRow__icon"/>
+            <h2 className="sidebarRow__title"> { title } </h2>
         </div>
     )
 } 
