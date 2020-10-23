c9f703b453ee30e5ae3b761a17e1be1a68dae4ba
vistritpandey <vistritpandey@gmail.com>
2020-10-23 09:10:20 +0530

    m

--- src/App.js
+++ src/App.js
@@ -3,15 +3,26 @@ import './App.css';
 import Header from './Header'
 import Sidebar from "./Sidebar"
 import Recommended from "./Recommended"
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <div className="app__page">
-      <Sidebar />
-      <Recommended />
-      </div>
+    <Router>
+    <Header />
+      <Switch>
+        <Route path="/search">
+          <h1>search page</h1>
+        </Route>
+        <Route path="/">
+          <div className="app__page">
+            <Sidebar />
+            <Recommended />
+          </div>
+        </Route>
+      </Switch>
+    </Router>
+    
     </div>
   );
 }
