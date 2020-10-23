cf3ea01b9e6f1e547a98f57685d918594fc5acaf
vistritpandey <vistritpandey@gmail.com>
2020-10-23 08:10:02 +0530

    m

--- src/Video.js
+++ src/Video.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import './Video.css'
 
-function Video() {
+function Video({image , title, channel, views, timestamp , channelImage }) {
     return (
         <div>
-            
+           <h4>Video</h4> 
         </div>
     )
 }
