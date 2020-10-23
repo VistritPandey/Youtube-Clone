398aead20ff5f66df0997901250dea998f7d8633
vistritpandey <vistritpandey@gmail.com>
2020-10-23 08:42:31 +0530

    m

--- src/Recommended.js
+++ src/Recommended.js
@@ -7,15 +7,88 @@ function Recommended() {
         <div className="recommended">
             <h2>Recommended</h2>
             <div className="recommeded__videos" >
-                <Video />
-                <Video />
-                <Video />
-                <Video />
-                <Video />
-                <Video />
-                <Video />
-                <Video />
-                <Video />
+                <Video 
+                    title="How to be pro at Nothing"
+                    views="99 Million Views"
+                    timestamp="9 days ago"
+                    channelImage="https://i.imgflip.com/2pby9j.jpg"
+                    channel="Small Paapu"
+                    image = "https://i.pinimg.com/originals/36/ff/c4/36ffc4c819df1591135ce1c6f0c0bea6.png"
+
+                />
+                <Video 
+                    title="How to be pro at Nothing"
+                    views="99 Million Views"
+                    timestamp="9 days ago"
+                    channelImage="https://i.imgflip.com/2pby9j.jpg"
+                    channel="Small Paapu"
+                    image = "https://i.pinimg.com/originals/36/ff/c4/36ffc4c819df1591135ce1c6f0c0bea6.png"
+
+                />
+                <Video 
+                    title="How to be pro at Nothing"
+                    views="99 Million Views"
+                    timestamp="9 days ago"
+                    channelImage="https://i.imgflip.com/2pby9j.jpg"
+                    channel="Small Paapu"
+                    image = "https://i.pinimg.com/originals/36/ff/c4/36ffc4c819df1591135ce1c6f0c0bea6.png"
+
+                />
+                <Video 
+                    title="How to be pro at Nothing"
+                    views="99 Million Views"
+                    timestamp="9 days ago"
+                    channelImage="https://i.imgflip.com/2pby9j.jpg"
+                    channel="Small Paapu"
+                    image = "https://i.pinimg.com/originals/36/ff/c4/36ffc4c819df1591135ce1c6f0c0bea6.png"
+
+                />
+                <Video 
+                    title="How to be pro at Nothing"
+                    views="99 Million Views"
+                    timestamp="9 days ago"
+                    channelImage="https://i.imgflip.com/2pby9j.jpg"
+                    channel="Small Paapu"
+                    image = "https://i.pinimg.com/originals/36/ff/c4/36ffc4c819df1591135ce1c6f0c0bea6.png"
+
+                />
+                <Video 
+                    title="How to be pro at Nothing"
+                    views="99 Million Views"
+                    timestamp="9 days ago"
+                    channelImage="https://i.imgflip.com/2pby9j.jpg"
+                    channel="Small Paapu"
+                    image = "https://i.pinimg.com/originals/36/ff/c4/36ffc4c819df1591135ce1c6f0c0bea6.png"
+
+                />
+                <Video 
+                    title="How to be pro at Nothing"
+                    views="99 Million Views"
+                    timestamp="9 days ago"
+                    channelImage="https://i.imgflip.com/2pby9j.jpg"
+                    channel="Small Paapu"
+                    image = "https://i.pinimg.com/originals/36/ff/c4/36ffc4c819df1591135ce1c6f0c0bea6.png"
+
+                />
+                <Video 
+                    title="How to be pro at Nothing"
+                    views="99 Million Views"
+                    timestamp="9 days ago"
+                    channelImage="https://i.imgflip.com/2pby9j.jpg"
+                    channel="Small Paapu"
+                    image = "https://i.pinimg.com/originals/36/ff/c4/36ffc4c819df1591135ce1c6f0c0bea6.png"
+
+                />
+                <Video 
+                    title="How to be pro at Nothing"
+                    views="99 Million Views"
+                    timestamp="9 days ago"
+                    channelImage="https://i.imgflip.com/2pby9j.jpg"
+                    channel="Paapu chota sa"
+                    image = "https://i.pinimg.com/originals/36/ff/c4/36ffc4c819df1591135ce1c6f0c0bea6.png"
+
+                />
+                
             </div>
         </div>
     )
--- src/Video.js
+++ src/Video.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import './Video.css'
+import Avatar from "@material-ui/core/Avatar"
 
 function Video({image , title, channel, views, timestamp , channelImage }) {
     return (
-        <div>
-           <h4>Video</h4> 
+        <div className="video">
+           <img src = {image} alt =""/>
+           <div className="video__info">
+              <Avatar className="video__avatar" alt={channel} src={channelImage}/>
+            <div className="video__text">
+                <h4>{title}</h4>
+                <p>{channel}</p>
+                <p>{views} • {timestamp}</p>
+            </div>
+           </div>
         </div>
     )
 }
